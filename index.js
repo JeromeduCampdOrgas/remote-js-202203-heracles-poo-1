2734a1d094bfb42c2823722f634569309c871170
Jerome <ducampdorgasjerome@gmail.com>
2022-03-07 12:14:18 +0100

    commit first

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 // First Labour : Heracles vs Nemean Lion
+import Fighter from "./src/Fighter.js";
 // use your Figher class here
+let fighters = [];
+let round = { roundNb: 0, attack: "" };
+const Heracles = new Fighter("🧔 Heracles", 20, 6);
+fighters.push(Heracles);
+
+const Nemean = new Fighter("🦁 Nemean", 11, 13);
+fighters.push(Nemean);
+let firstAttack = Math.floor(Math.random() * 2);
+round.attack = firstAttack;
+do {
+  console.log(JSON.stringify(round));
+  switch (round.attack) {
+    case 1:
+      fighters[0].fight(fighters[1]);
+      round.roundNb += 1;
+      round.attack = 0;
+      console.log(fighters[0]);
+      console.log(fighters[1]);
+      console.log("🕛 " + round.roundNb);
+      break;
+    case 0:
+      fighters[1].fight(fighters[0]);
+      round.roundNb += 1;
+      round.attack = 1;
+      console.log(fighters[0]);
+      console.log(fighters[1]);
+      console.log("🕛 " + round.roundNb);
+  }
+} while (Heracles.life > 0 && Nemean.life > 0);
+fighters[0].life == 0
+  ? console.log("💀" + fighters[0].name + " : " + fighters[0].life)
+  : console.log("🏆" + fighters[0].name + " : " + fighters[0].life);
+
+fighters[1].life == 0
+  ? console.log("💀" + fighters[1].name + " : " + fighters[1].life)
+  : console.log("🏆" + fighters[1].name + " : " + fighters[1].life);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,16 @@
+const MAX_LIFE = 100;
 /* Fighter class definition */
+export default class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+  fight(enemy) {
+    let attack = parseInt(Math.random() * (this.strength - 1) + 1);
+    let defense = parseInt(Math.random() * (enemy.dexterity - 1) + 1);
+    let damage = attack - defense > 0 ? attack - defense : 0;
+    enemy.life = enemy.life - damage < 0 ? 0 : enemy.life - damage;
+  }
+}
